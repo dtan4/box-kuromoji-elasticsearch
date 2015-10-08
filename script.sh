e6490bcdfd01cec19f237b6e0a835816432de724
Daisuke Fujita <dtanshi45@gmail.com>
2015-10-08 09:55:56 +0900

    Pass HOST as environment variable

--- script.sh
+++ script.sh
@@ -2,6 +2,7 @@
 
 ELASTICSEARCH_VERSION=1.5.2
 KUROMOJI_VERSION=2.5.0
+BOX_HOST=${BOX_HOST:-192.168.0.1}
 
 sudo apt-get update
 sudo apt-get -y upgrade
@@ -9,7 +10,7 @@ sudo apt-get install -y openjdk-7-jre wget
 
 sudo wget -O /tmp/elasticsearch-$ELASTICSEARCH_VERSION.deb https://download.elasticsearch.org/elasticsearch/elasticsearch/elasticsearch-$ELASTICSEARCH_VERSION.deb
 sudo dpkg -i /tmp/elasticsearch-$ELASTICSEARCH_VERSION.deb
-sudo sed -i 's/#http.enabled: false/http.enabled: true/g' /etc/elasticsearch/elasticsearch.yml
-sudo sed -i 's/#network.host: 192.168.0.1/network.host: $$HOST$$/g' /etc/elasticsearch/elasticsearch.yml
+sudo sed -i "s/#http.enabled: false/http.enabled: true/g" /etc/elasticsearch/elasticsearch.yml
+sudo sed -i "s/#network.host: 192.168.0.1/network.host: $BOX_HOST/g" /etc/elasticsearch/elasticsearch.yml
 sudo /usr/share/elasticsearch/bin/plugin -install elasticsearch/elasticsearch-analysis-kuromoji/$KUROMOJI_VERSION
 sudo service elasticsearch restart
